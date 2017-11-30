2d9588753b49e09093f8d8d2ad5434bdfe255526
kristiankolthoff <k_kolthoff@web.de>
2017-11-30 03:02:51 +0100

    Postalcode comparators and phone sift update
    Former-commit-id: d12001ce9346be2bd52ca5554f1b7b191fe2f021

--- src/main/java/de/unima/webdataintegration/location/identityresolution/comparators/LocationPhoneSift4Comparator.java
+++ src/main/java/de/unima/webdataintegration/location/identityresolution/comparators/LocationPhoneSift4Comparator.java
@@ -8,7 +8,6 @@ import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
-import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
 import de.unima.webdataintegration.location.model.Location;
 import info.debatty.java.stringsimilarity.experimental.Sift4;
 
--- src/main/java/de/unima/webdataintegration/location/identityresolution/comparators/LocationPostalCodeComparator.java
+++ src/main/java/de/unima/webdataintegration/location/identityresolution/comparators/LocationPostalCodeComparator.java
@@ -1,7 +1,5 @@
 package de.unima.webdataintegration.location.identityresolution.comparators;
 
-import java.util.Objects;
-
 import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
@@ -23,8 +21,8 @@ public class LocationPostalCodeComparator implements Comparator<Location, Attrib
 	@Override
 	public double compare(Location record1, Location record2,
 			Correspondence<Attribute, Matchable> schemaCorrespondence) {
-		if(Objects.isNull(record1.getPostalCode()) || Objects.isNull(record2.getPostalCode())) {
-			return 0;
+		if(!record1.hasValue(Location.POSTAL_CODE) || !record2.hasValue(Location.POSTAL_CODE)) {
+			return -1;
 		}
 		//Normalize streetAddress1
 		String streetAddress1 = record1.getPostalCode().trim();

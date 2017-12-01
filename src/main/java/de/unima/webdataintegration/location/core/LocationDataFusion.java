3ce95ddbe684d1a2dfb2fa0d4687906eb68499c7
kkolthof <k_kolthoff@web.de>
2017-12-01 14:15:04 +0100

    Data fusion setup
    Former-commit-id: c3c150afccee626c363075aa4f7762924ddd99a3

--- src/main/java/de/unima/webdataintegration/location/core/LocationDataFusion.java
+++ src/main/java/de/unima/webdataintegration/location/core/LocationDataFusion.java
@@ -9,23 +9,30 @@ import java.time.temporal.ChronoField;
 import java.util.Locale;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.xpath.XPathExpressionException;
 
 import org.xml.sax.SAXException;
 
 import de.uni_mannheim.informatik.dws.winter.datafusion.CorrespondenceSet;
 import de.uni_mannheim.informatik.dws.winter.datafusion.DataFusionEngine;
+import de.uni_mannheim.informatik.dws.winter.datafusion.DataFusionEvaluator;
 import de.uni_mannheim.informatik.dws.winter.datafusion.DataFusionStrategy;
+import de.uni_mannheim.informatik.dws.winter.model.DataSet;
 import de.uni_mannheim.informatik.dws.winter.model.FusibleDataSet;
 import de.uni_mannheim.informatik.dws.winter.model.FusibleHashedDataSet;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroupFactory;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.unima.webdataintegration.location.fusion.fusers.LongitudeFuserAverage;
+import de.unima.webdataintegration.location.fusion.rules.LongitudeEvaluationRule;
 import de.unima.webdataintegration.location.model.Location;
+import de.unima.webdataintegration.location.model.LocationXMLFormatter;
 import de.unima.webdataintegration.location.model.LocationXMLReader;
 
 public class LocationDataFusion {
 
 	public static void main(String[] args) throws XPathExpressionException, 
-			ParserConfigurationException, SAXException, IOException {
+			ParserConfigurationException, SAXException, IOException, TransformerException {
 		//Load Prinz dataset
 		FusibleHashedDataSet<Location, Attribute> datasetPrinz = new FusibleHashedDataSet<>();
 		new LocationXMLReader().loadFromXML(new File("src/main/resources/data/input/prinz_locations_with_hours.xml"), 
@@ -61,15 +68,36 @@ public class LocationDataFusion {
 		datasetYelp.setDate(LocalDateTime.parse("2015-01-01", formatter));
 		datasetTripadvisor.setDate(LocalDateTime.parse("2013-01-01", formatter));
 		
-//		CorrespondenceSet<Location, Attribute> correspondences = new CorrespondenceSet<>();
-//		correspondences.loadCorrespondences(new File("src/main/resources/data/goldstandard/results/yelp_dupl.csv"), locationsYelp, locationsYelp2);
+		//Load correspondence sets
+		CorrespondenceSet<Location, Attribute> correspondences = new CorrespondenceSet<>();
+		correspondences.loadCorrespondences(new File("src/main/resources/data/correspondences/prinz_yelp_corres.csv"),
+				datasetPrinz, datasetYelp);
+		correspondences.loadCorrespondences(new File("src/main/resources/data/correspondences/yelp_tripadvisor_corres.csv"),
+				datasetYelp, datasetTripadvisor);
 		
-//		//Specify data fusion strategy
-//		DataFusionStrategy<Location, Attribute> strategy = new DataFusionStrategy<>(new LocationXMLReader());
-////		strategy.addAttributeFuser(Location.LONGITUDE, new LongitudeFuserAverage(), new LongitudeEvaluationRule());
-//		
-//		DataFusionEngine<Location, Attribute> engine = new DataFusionEngine<>(strategy);
-//		@SuppressWarnings("unused")
-//		FusibleDataSet<Location, Attribute> fusedDataSet = engine.run(correspondences, null);
+		//Print correspondence group size distribution
+		correspondences.printGroupSizeDistribution();
+		
+		//Specify data fusion strategy
+		DataFusionStrategy<Location, Attribute> strategy = new DataFusionStrategy<>(new LocationXMLReader());
+		strategy.addAttributeFuser(Location.LONGITUDE, new LongitudeFuserAverage(), new LongitudeEvaluationRule());
+		
+		//Run data fusion process
+		DataFusionEngine<Location, Attribute> engine = new DataFusionEngine<>(strategy);
+		FusibleDataSet<Location, Attribute> fusedDataSet = engine.run(correspondences, null);
+		
+		new LocationXMLFormatter().writeXML(new File("src/main/resources/data/fusion/fused.xml"), fusedDataSet);
+		
+		//Load fusion goldstandard
+		DataSet<Location, Attribute> goldstandard = new FusibleHashedDataSet<>();
+		new LocationXMLReader().loadFromXML(new File("src/main/resources/data/goldstandard/fused.xml"), 
+				"locations/location", goldstandard);
+		
+		//Evaluate data fusion strategy
+		DataFusionEvaluator<Location, Attribute> evaluator = new DataFusionEvaluator<>(strategy, 
+				new RecordGroupFactory<>());
+		evaluator.setVerbose(true);
+		double accuarcy = evaluator.evaluate(fusedDataSet, goldstandard, null);
+		System.out.println("Accuarcy : " + accuarcy);
 	}
 }

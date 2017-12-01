ef28e8308035979b486e878723fd9b1445539479
kristiankolthoff <k_kolthoff@web.de>
2017-12-01 23:15:44 +0100

    Last
    Former-commit-id: 90743e363bd0b292153a6e8aa1c7996e50acda9c

--- src/main/java/de/unima/webdataintegration/location/fusion/rules/RatingEvaluationRule.java
+++ src/main/java/de/unima/webdataintegration/location/fusion/rules/RatingEvaluationRule.java
@@ -4,13 +4,17 @@ import de.uni_mannheim.informatik.dws.winter.datafusion.EvaluationRule;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.Matchable;
 import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.similarity.numeric.PercentageSimilarity;
 import de.unima.webdataintegration.location.model.Location;
 
 public class RatingEvaluationRule extends EvaluationRule<Location, Attribute>{
 
+	private static PercentageSimilarity similarity =
+			new PercentageSimilarity(10);
+	
 	@Override
 	public boolean isEqual(Location record1, Location record2, Attribute schemaElement) {
-		return record1.getRating() == record2.getRating();
+		return similarity.calculate(record1.getRating(), record2.getRating()) > 0.8 ? true : false;
 	}
 
 	@Override
